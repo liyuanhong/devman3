2fc712295b93db69b1dc659c9c7ec0c311ab191f
liyuanhong <908963295@qq.com>
2018-02-08 20:26:19 +0800

    我的页面界面调整

--- application/views/userCenter/myPage.php
+++ application/views/userCenter/myPage.php
@@ -9,12 +9,38 @@
 
 
 ?>
-<div class="content-wrapper" style="background-color: #FFCCCC;">
-    <div style="width: 100%;height:300px;background-color: #999999;">
-        <div style="width: 90%;height: 200px;margin-left: 5%;background-color: snow;padding-top: 50px;">
-            <div>
-                <img src="" style="width: 120px;height: 120px;background-color: #00a7d0;">
+<link rel="stylesheet" href="<?php echo  'http://'.ROOT_URL ?>static/devman3/css/userCenter/myPage.css">
+<script src="<?php echo  'http://'.ROOT_URL ?>static/devman3/js/userCenter/mypage.js"></script>
+
+<div class="content-wrapper" style="_background-color: #FFCCCC;">
+    <div style="width: 100%;height:300px;_background-color: #999999;">
+        <i class="fa fa-fw fa-edit" style="margin-right:150px;margin-top:10px;font-size: 25px;float: right;"></i>
+        <div style="width: 90%;height: 200px;margin-left: 5%;_background-color: snow;padding-top: 50px;">
+            <div style="padding: 1px;width: 124px;height: 124px;border-style: solid;border-width: 1px;display: inline;float: left;border-color: #9d9d9d;">
+                <i class="fa fa-fw fa-edit" style="position: absolute;margin-top: 100px;margin-left: 95px;font-size: 25px;"></i>
+                <img id="head_portrait"src="<?php echo  'http://'.ROOT_URL.'static/dist/img/user2-160x160-2.jpg';?>" style="width: 120px;height: 120px;_background-color: #9d9d9d;">
             </div>
+            <div style="float: left;padding-top: 5px;padding-left: 10px;width: 80%;">
+                <label style="font-size: 18px;">李远洪</label>
+            </div>
+            <div style="float: left;padding-top: 5px;padding-left: 10px;width:80%;">
+                <label>角色：系统管理员</label><label>|</label>
+                <label>上次登录：2017-12-12</label><label>|</label>
+                <label>email：678975436@qqcom</label><label>|</label>
+                <label>手机：13566678897</label><label>|</label>
+                <label>qq：9999999999</label><label>|</label>
+                <label>部门：新媒体产品部</label>
+            </div>
+            <div style="float: left;margin-left: 10px;width:80%;height: 1px;background-color: red;"></div>
+            <div style="float: left;padding-top: 5px;padding-left: 10px;width:80%;">
+                <p>我是一个性格开朗，善于沟通，富有幽默干的年轻人，喜欢专研的一代。我是一个性格开朗，善于沟通，富有幽默干的年轻人，喜欢专研的一代。我是一个性格开朗，善于沟通，富有幽默干的年轻人，喜欢专研的一代。我是一个性格开朗，善于沟通，富有幽默干的年轻人，喜欢专研的一代。</p>
+            </div>
+
+        </div>
+
+        <div style="width: 90%;height: 200px;margin-left: 5%;background-color: snow;margin-top: 50px;">
+
+
         </div>
     </div>
 
